320d1b07e1ea621cfa6ab54920f766014c63a0ab
SrivardhanVarmaM <150221942+zorojuro2799@users.noreply.github.com>
2025-09-27 04:33:58 +0530

    start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-sudo apt-get update
-
-sudo apt-get install -y tesseract-ocr libtesseract-dev libleptonica-dev pkg-config
-
-streamlit run app.py --server.port $PORT --server.enableCORS false
